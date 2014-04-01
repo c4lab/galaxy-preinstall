25c3898882e7dc4ef6ff3f37ee19dec84617b873
Kuan-Yi Li <kyli.tw@gmail.com>
2014-04-02 01:11:12 +0800

    Remove unused repositories

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ RHEL
 Add extra repositories:
 
 ```
-$ curl https://raw.github.com/jlhg/galaxy-preinstall/master/add_extra_repo.sh |sudo bash
-$ sudo yum install yum-priorities
+$ sudo yum install yum-plugin-priorities
+$ curl https://raw.github.com/jlhg/galaxy-preinstall/master/add_extra_repo.sh | sudo bash
 ```
 
 Set the priorities of CentOS-Base and EPEL repos:
 
-* Add `priority=1` to each section in `/etc/yum.repos.d/CentOS-Base.repo`.
+* Add `priority=10` to each section in `/etc/yum.repos.d/CentOS-Base.repo`.
 
   E.g.
 
@@ -21,7 +21,7 @@ mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo
 #baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-priority=1
+priority=10
 
 #released updates
 [updates]
@@ -30,13 +30,13 @@ mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo
 #baseurl=http://mirror.centos.org/centos/$releasever/updates/$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-priority=1
+priority=10
 
 ...
 
 ```
 
-* Add `priority=10` to each section in `/etc/yum.repos.d/epel.repo`.
+* Add `priority=20` to each section in `/etc/yum.repos.d/epel.repo`.
 
 Update packages:
 
--- add_extra_repo.sh
+++ add_extra_repo.sh
@@ -1,17 +1,21 @@
 #/bin/bash
 # http://tecadmin.net/top-5-yum-repositories-for-centos-rhel-systems/
 
-# EPEL
-rpm -Uvh http://mirror01.idc.hinet.net/EPEL/6/x86_64/epel-release-6-8.noarch.rpm
+# Extra Packages for Enterprise Linux (EPEL)
+# tmux, htop, bash-completion, ...
+rpm --upgrade --verbose --hash http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
-# RPMForge
-rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+# Auxiliary repositories should be added if, and only if, required package exists just in not-already-added repositories.
+# yum-plugin-priorities should also be installed and configured to regulate priorities of packages that exist in multiple repositories.
 
-# REMI
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-
-# CentAlt
-rpm -Uvh http://centos.alt.ru/repository/centos/6/x86_64/centalt-release-6-1.noarch.rpm
-
-# Webtatic
-rpm -Uvh http://repo.webtatic.com/yum/el6/latest.rpm
+## RepoForge
+#rpm --upgrade --verbose --hash http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+#
+## REMI
+#rpm --upgrade --verbose --hash http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+#
+## CentAlt
+#rpm --upgrade --verbose --hash http://centos.alt.ru/repository/centos/6/x86_64/centalt-release-6-1.noarch.rpm
+#
+## Webtatic
+#rpm --upgrade --verbose --hash http://repo.webtatic.com/yum/el6/latest.rpm
